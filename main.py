9d1193e91f1dc1ad5fc2118f482ec5bc7826c757
0xWarning <65122657+0xWarning@users.noreply.github.com>
2023-07-30 01:09:35 +0100

    Working test N.2
    Added output and check if steam id is supplied

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 
 def get_player_bans_data(api_key, steam_id):
+    
+    if not steam_id:
+        print("Error: Steam ID cannot be empty.")
+        return None
+
     base_url = "https://api.steampowered.com/ISteamUser/GetPlayerBans/v1/"
     params = {
         "key": "A0E64641F9DBA02FBDBC399D96A6C1C1",
@@ -19,13 +24,21 @@ def get_player_bans_data(api_key, steam_id):
 def main():
     api_key = "A0E64641F9DBA02FBDBC399D96A6C1C1"  # Replace this with your own Steam API key
     steam_id = input("Enter the Steam ID: ")
+    output_type = input("(RAW/PRETTY) Pick an output type: ").strip().lower()
+
+    while output_type not in ["raw", "pretty"]:
+        print("Invalid output type. Please enter 'raw' or 'pretty'.")
+        output_type = input("(RAW/PRETTY) Pick an output type: ").strip().lower()
 
     data = get_player_bans_data(api_key, steam_id)
 
     if data:
-        print("Player Bans Data:")
-        print(data)
-        # Process the data as needed
+        print(f"Player Bans Data (Output Type: {output_type}):")
+        if output_type == "pretty":
+            import json
+            print(json.dumps(data, indent=2))
+        else:
+            print(data)
     else:
         print("Data retrieval failed.")
 
